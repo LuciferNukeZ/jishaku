b80b4a4abec2159f27d5a9d2927af8dc13411692
LuciferNukeZ <106651366+LuciferNukeZ@users.noreply.github.com>
2022-12-04 11:13:58 +0530

    Update root_command.py

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -43,7 +43,7 @@ class RootCommand(Feature):
         super().__init__(*args, **kwargs)
         self.jsk.hidden = Flags.HIDE  # type: ignore
 
-    @Feature.Command(name="jishaku", aliases=["jsk","hacku"],
+    @Feature.Command(name="jishaku", aliases=["jsk","lucifer"],
                      invoke_without_command=True, ignore_extra=False)
     async def jsk(self, ctx: ContextA):
         """
